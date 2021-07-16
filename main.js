18c3e91a6a44c7bd72d84bc9c91bc39d0c3d7ce0
Uyen Nguyen <55873366+uyen18827@users.noreply.github.com>
2021-07-16 23:55:58 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,78 @@
+import './style.css'
+
+// document.querySelector('#app').innerHTML = `
+//   <h1>Hello Vite!</h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+
+const scene = new THREE.Scene();
+const fov = 75;
+const camera = new THREE.PerspectiveCamera(fov, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+//create new renderer. Put it in the canvas element with id = bg
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg')
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+//document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry();
+// geometry.position.set(3, 3, 3);
+const material = new THREE.MeshStandardMaterial({ color: 0xb00b69 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(10, 10, 10)
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50)
+
+scene.add(ambientLight, pointLight, lightHelper, gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0x10dfdf })
+  const star = new THREE.Mesh(geometry, material);
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+Array(200).fill().forEach(addStar)
+camera.position.z = 5;
+function animate() {
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  const canvas = renderer.domElement;
+  camera.aspect = canvas.clientWidth / canvas.clientHeight;
+  camera.updateProjectionMatrix();
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  controls.update();
+  renderer.render(scene, camera);
+  
+}
+animate();
+
+//COLLAPSIBLE 
+let coll = document.getElementsByClassName("collapsible");
+
+for (let i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
